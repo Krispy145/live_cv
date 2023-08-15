d885c32f48333a283e74c5a0d00628c77dedb883
David <davidkisbeygreen145@gmail.com>
2023-08-15 22:12:38 +0100

    Supabase running on local client

--- live_cv_serverpod_flutter/lib/config/flavors/main_dev.dart
+++ live_cv_serverpod_flutter/lib/config/flavors/main_dev.dart
@@ -2,11 +2,13 @@ import 'package:live_cv_serverpod_client/live_cv_serverpod_client.dart';
 import 'package:live_cv_serverpod_flutter/config/flavors/flavor_config.dart';
 import 'package:live_cv_serverpod_flutter/main_entry.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+void main() async {
   final FlavorConfig flavorConfig = FlavorConfig(
     environment: Environment.dev,
     client: Client('http://localhost:8080/')..connectivityMonitor = FlutterConnectivityMonitor(),
   );
+  setPathUrlStrategy();
   mainEntry(flavorConfig: flavorConfig);
 }
--- live_cv_serverpod_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ live_cv_serverpod_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import cloud_firestore
 import connectivity_plus
 import firebase_core
 import path_provider_foundation
+import shared_preferences_foundation
+import sign_in_with_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
